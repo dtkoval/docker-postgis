bba86032dc322c6d1572d5ae9e69cffeec501d57
dtkoval <d.koval.work@gmail.com>
2017-01-23 14:51:09 +0300

    added extentions

--- initdb-postgis.sh
+++ initdb-postgis.sh
@@ -17,7 +17,10 @@ for DB in template_postgis "$POSTGRES_DB"; do
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
+		CREATE EXTENSION IF NOT EXISTS postgis_sfcgal;
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+		CREATE EXTENSION IF NOT EXISTS address_standardizer;
+		CREATE EXTENSION IF NOT EXISTS address_standardizer_data_us;
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
 done
